adfaae5ca6fc108616dfadf9f760db6d7ba3bf90
Joel <jzarley@umail.ucsb.edu>
2016-11-04 01:18:22 -0700

    hello, World

---
+++ helloWorld.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+//without this you must write std:: before std commands
+using namespace std;
+
+int main(){
+
+  cout << "Hello, World!" << endl;
+
+  //not neccesary in C++ (0=success exit status)
+  return 0;
+}
